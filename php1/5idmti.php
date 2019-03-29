a111bbfc84bc3f647221308eb1fadbaa6efaea8b
haifa <h.lakhoua@gmail.com>
2019-03-29 20:06:48 +0100

    fgghbjj

--- php1/5idmti.php
+++ php1/5idmti.php
@@ -4,3 +4,24 @@ $nom=$_REQUEST['description'];
 echo "$nom";
 ?>
 
+<?php
+$nom=$_REQUEST['nom'];
+echo "$nom";
+?>
+<?php
+$nom=$_REQUEST['prenom'];
+echo "$nom";
+?>
+<?php
+$nom=$_REQUEST['telephone'];
+echo "$nom";
+?>
+<?php
+$nom=$_REQUEST['email'];
+echo "$nom";
+?>
+<?php
+$nom=$_REQUEST['date de naissance'];
+echo "$nom";
+?>
+
