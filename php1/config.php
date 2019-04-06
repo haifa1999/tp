b2f47264ff280ba94ead1f7db83873ae58d04be0
haifa <h.lakhoua@gmail.com>
2019-04-07 00:09:02 +0100

    dfghj,;

--- php1/config.php
+++ php1/config.php
@@ -1,15 +1,15 @@
 <?php
 function connect(){
 $server="localhost";
-$base="isg";
+$base="hayfoufa";
 $username="root";
 $pass="";
 try{
-    //code a  executer ...
+    
     $con=new PDO("mysql:host=$server;dbname=$base",$username,$pass);//con:tconnectilna aal base de donne..
     return $con;
 }catch ( PDOExeption $e){
-    //traitement en cas d'<exeption...
+    
     die("erreeeeeur: ".$e->getMessage());
 }
 }
