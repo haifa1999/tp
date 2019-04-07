125e566d64ccf303e18700674843c40473668c9b
haifa <h.lakhoua@gmail.com>
2019-04-07 15:37:46 +0100

    ghjk

--- php1/5idmti.php
+++ php1/5idmti.php
@@ -1,24 +1,6 @@
 
 <?php
 $description=$_REQUEST['description'];
-echo "description";
-
-$nom=$_REQUEST['nom'];
-echo "$nom";
-
-$prenom=$_REQUEST['prenom'];
-echo "$prenom";
-
-$telephone=$_REQUEST['telephone'];
-echo "$telephone";
-
-$email=$_REQUEST['email'];
-echo "$email";
-
-$datedenaissance=$_REQUEST['date de naissance'];
-echo "$datedenaissance";
-$motdepasse=$_REQUEST['motdepasse']
-echo"$motdepasse"
-
+echo "$description";
 ?>
 
---
+++ php1/5idmti2.php
@@ -0,0 +1,19 @@
+<?php
+
+$nom=$_REQUEST['nom'];
+echo "$nom";
+
+$prenom=$_REQUEST['prenom'];
+echo "$prenom";
+
+$telephone=$_REQUEST['telephone'];
+echo "$telephone";
+
+$email=$_REQUEST['email'];
+echo "$email";
+
+$datedenaissance=$_REQUEST['date de naissance'];
+echo "$datedenaissance";
+$password=$_REQUEST['motdepasse'];
+echo"$password";
+?>
